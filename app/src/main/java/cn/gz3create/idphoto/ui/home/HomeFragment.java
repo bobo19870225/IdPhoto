1e93b8171cbae4def291b5142f45ce0ff9f74902
“卢声波” <rain19870225@163.com>
2020-05-23 16:35:27 +0800

    Initial commit

---
+++ app/src/main/java/cn/gz3create/idphoto/ui/home/HomeFragment.java
@@ -0,0 +1,49 @@
+package cn.gz3create.idphoto.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import cn.gz3create.idphoto.BR;
+import cn.gz3create.idphoto.R;
+import cn.gz3create.idphoto.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+    private FragmentHomeBinding mViewDataBinding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+
+        mViewDataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false);
+        mViewDataBinding.setVariable(BR.model, homeViewModel);
+        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+            }
+        });
+        return mViewDataBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        mViewDataBinding.album.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+}
